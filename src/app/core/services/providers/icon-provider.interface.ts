0b11be721c460c2218db3724c83030b36497286d
Sharad Chandel <sharadchandel2005@gmail.com>
2026-01-30 17:37:07 +0530

    feat(core): create IconProvider interface

---
+++ src/app/core/services/providers/icon-provider.interface.ts
@@ -0,0 +1,22 @@
+// src\app\core\services\providers\icon-provider.interface.ts
+
+import { Observable } from 'rxjs';
+import { IconApiResponse } from '../../models/icon.model';
+
+export interface IconProvider {
+  readonly name: string;
+  readonly displayName: string;
+  readonly baseUrl: string;
+  readonly iconPath: string;
+
+  search(
+    query: string,
+    limit: number,
+    offset: number,
+  ): Observable<IconApiResponse>;
+  
+  getRandom(
+    limit: number,
+    offset: number,
+  ): Observable<IconApiResponse>;
+}
