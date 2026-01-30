e0ef7237b554267a786bb0912d4a39eeba8add7c
Sharad Chandel <sharadchandel2005@gmail.com>
2026-01-30 17:52:30 +0530

    feat(components): add LoadingSpinnerComponent

---
+++ src/app/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,12 @@
+// src\app\components\loading-spinner\loading-spinner.component.ts
+
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  templateUrl: './loading-spinner.component.html',
+  styleUrls: ['./loading-spinner.component.css'],
+})
+export class LoadingSpinnerComponent {
+  @Input() message = 'Loading...';
+}
