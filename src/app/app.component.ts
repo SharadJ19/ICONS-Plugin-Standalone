4f1fa704f1df38274fbd79afc87e064a1f54a352
Sharad Chandel <sharadchandel2005@gmail.com>
2026-01-30 17:32:58 +0530

    chore(init): initialize Angular 16 project with basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+// src\app\app.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { IconAggregatorService } from './core/services/icon-aggregator.service';
+import { IconoirProviderService } from './core/services/providers/iconoir.provider.service';
+import { BootstrapProviderService } from './core/services/providers/bootstrap.provider.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'icons-plugin-standalone';
+export class AppComponent implements OnInit {
+  constructor(
+    private iconAggregator: IconAggregatorService,
+    private iconoirProvider: IconoirProviderService,
+    private bootstrapProvider: BootstrapProviderService,
+  ) {}
+
+  ngOnInit(): void {
+    this.iconAggregator.registerProviders([
+      this.iconoirProvider,
+      this.bootstrapProvider,
+    ]);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
+// src\app\app.module.ts
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material Modules
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { IconCardComponent } from './components/icon-card/icon-card.component';
+import { ProviderSelectorComponent } from './components/provider-selector/provider-selector.component';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IconCardComponent,
+    ProviderSelectorComponent,
+    LoadingSpinnerComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatFormFieldModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
