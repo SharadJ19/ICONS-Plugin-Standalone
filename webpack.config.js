247bd2dcdb6201f0ead6f09a0b472f78684fdb5c
Sharad Chandel <sharadchandel2005@gmail.com>
2026-02-02 21:10:22 +0530

    chore: add webpack configuration setup for module federation

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
+// webpack.config.js
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
-    const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
-
-    module.exports = withModuleFederationPlugin({
-    
-      name: 'icons-plugin-standalone',
-    
-      exposes: {
-        './AppModule': './src/app/app.module.ts'
-      },
-    
-      shared: {
-        ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-      },
-    });
+module.exports = withModuleFederationPlugin({
+  name: 'icons-plugin-standalone',
+  filename: 'remoteEntry.js',
+  
+  exposes: {
+    './QlPluginModule': './src/app/ql-plugin/ql-plugin.module.ts',
+    './QlDefaultPluginComponent': './src/app/ql-plugin/ql-default-plugin/ql-default-plugin.component.ts',
+    './HomeComponent': './src/app/pages/home/home.component.ts'
+  },
+  
+  shared: {
+    ...shareAll({ 
+      singleton: true, 
+      strictVersion: false, 
+      requiredVersion: 'auto' 
+    }),
+  },
+  
+  sharedMappings: []
+});
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,1 +1,2 @@
+// webpack.prod.config.js - Option 2 (Simpler)
 module.exports = require('./webpack.config');
