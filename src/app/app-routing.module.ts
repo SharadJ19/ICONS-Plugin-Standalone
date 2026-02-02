cba5d108b611c652512e8dc255ee90a60361c65c
Sharad Chandel <sharadchandel2005@gmail.com>
2026-02-02 12:51:21 +0530

    chore: remove wildcard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
